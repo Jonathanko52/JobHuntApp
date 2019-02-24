f78e895ae00f1fab625ae4760fce10ee27a0420e
Jonathan Ko <jonathanko@Jonathans-MacBook-Pro.local>
2019-02-23 19:48:21 -0800

    graphql and mongodb implentation

---
+++ server/server.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const bcrypt = require ('bcryptjs');
+
+const Event = require('./models/events.js')
+const User = require('./models/users.js')
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+      creator: User!
+    }
+    type User {
+      _id: ID!
+      email: String!
+      password: String
+      createdEvents: [Event!]
+    }
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+    input UserInput {
+      email: String!
+      password: String!
+    }
+    type RootQuery {
+        events: [Event!]!
+    }
+    type RootMutation {
+        createEvent(eventInput: EventInput): Event
+        createUser(userInput: UserInput): User
+    }
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`),
+    rootValue: {
+      events: () => {
+          return Event.find()
+          .then(events =>{
+            return events.map(result=>{
+                return {
+                    ...result._doc, 
+                    _id: result._id
+                }
+            })
+          })
+          .catch(err=> {
+              throw err
+            })
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        })
+        return event.save(
+        ).then( result =>{
+            console.log(result._doc)
+        }).catch( err =>{
+            console.log(err)
+        });
+    },
+      createUser: args => {
+        return bcrypt
+            .hash(args.userInput.password, 12)
+            .then(hashed =>{
+                const user = new User({
+                    email: args.userInput.email,
+                    password:hashed 
+                })
+                console.log('Creating new User', user)
+            return user.save();
+            })
+            .then(result =>{
+                console.log('Cuser created', result)
+                return {...result._doc, _id: result.id}
+             })
+            .catch(err=>{
+                throw err
+            })
+      },
+    },
+
+    graphiql: true
+  })
+);
+
+    mongoose.connect(
+        `mongodb+srv://JonathanKo523:upYmpvNRCyVf4gT@jobhuntapp-lgaer.mongodb.net/events-react-dev?retryWrites=true`
+    ) .then(()=>{
+        console.log('applistening')
+        app.listen(3100)
+    }).catch(err => {
+        console.log(err)
+    })
+
