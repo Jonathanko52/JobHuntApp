d6807f2fb2255b0bc01cbd348bdb3f7ac3ae88df
Jonathan Ko <jonathan.ko523@gmail.com>
2019-09-03 13:40:58 -0700

    git desktop disabled

--- src/server/server.js
+++ src/server/server.js
@@ -18,6 +18,8 @@ app.get("/RetrieveHtmlLinkedIn/:link", taskController.retrieveHtmlLinkedIn);
 
 app.get("/RetrieveHtmlIndeed/:link", taskController.retrieveHtmlIndeed);
 
+//test
+
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
