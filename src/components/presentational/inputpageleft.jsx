328fe32a96e135db379b8a201db265f8fd96d5ca
Jonathan Ko <jonathan.ko523@gmail.com>
2021-04-12 22:12:15 -0700

    Refactoring/Breaking components into components

--- src/components/presentational/inputpageleft.jsx
+++ src/components/presentational/inputpageleft.jsx
@@ -1,75 +1,11 @@
 import React from "react";
+import ManualSubmitForm from "./manualSubmitForm.jsx";
 
 const LeftPage = (props) => {
-  // let total = "N/A";
-  // if (props.totalJobsFromSheets > 0) {
-  //   total = props.totalJobsFromSheets;
-  // }
   //Input boxes that take in input regarding relevant information to a job application.
   return (
     <div className="leftBar col-xs-6">
-      <p className="text-center mt-3">
-        Fill the fields below to add a job posting to the list
-      </p>
-      <div className="LeftSubBar">
-        <h4>Website</h4>
-        <select
-          ref={props.webRef}
-          className="WebsiteInput TaskInput"
-          onChange={(e) => {
-            props.handleChangeWebsite(e);
-          }}>
-          <option value="Angelist">Angelist</option>
-          <option value="Indeed">Indeed</option>
-          <option defaultValue="selected" value="LinkedIn">
-            LinkedIn
-          </option>
-        </select>
-        <h4>Company</h4>
-        <input
-          ref={props.compRef}
-          className="CompanyInput TaskInput"
-          onChange={(e) => {
-            props.handleChangeCompany(e);
-          }}
-        />
-        <h4>Title</h4>
-        <input
-          ref={props.titleRef}
-          className="TitleInput TaskInput"
-          onChange={(e) => {
-            props.handleChangeTitle(e);
-          }}
-        />
-        <h4>Location</h4>
-        <input
-          ref={props.locRef}
-          className="LocationInput TaskInput"
-          onChange={(e) => {
-            props.handleChangeLocation(e);
-          }}
-        />
-        <h4>Link</h4>
-        <input
-          ref={props.linkRef}
-          className="LinkInput TaskInput"
-          onChange={(e) => {
-            props.handleChangeLink(e);
-          }}
-        />
-        <button
-          className="btn btn-primary"
-          onClick={() => {
-            props.addToList();
-            props.webRef.current.value = "LinkedIn";
-            props.compRef.current.value = "";
-            props.titleRef.current.value = "";
-            props.locRef.current.value = "";
-            props.linkRef.current.value = "";
-          }}>
-          Add
-        </button>
-      </div>
+      <ManualSubmitForm></ManualSubmitForm>
       <h3 className="text-center"> OR </h3>
       <p className="text-center mt-4">
         Copy and paste the url of the job page (Currently supports LinkedIn and
---
+++ src/components/presentational/manualSubmitForm.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+const ManualSubmitForm = (props) => {
+  return (
+    <div>
+      <p className="text-center mt-3">
+        Fill the fields below to add a job posting to the list
+      </p>
+      <div className="LeftSubBar">
+        <h4>Website</h4>
+        <select
+          ref={props.webRef}
+          className="WebsiteInput TaskInput"
+          onChange={(e) => {
+            props.handleChangeWebsite(e);
+          }}>
+          <option value="Angelist">Angelist</option>
+          <option value="Indeed">Indeed</option>
+          <option defaultValue="selected" value="LinkedIn">
+            LinkedIn
+          </option>
+        </select>
+        <h4>Company</h4>
+        <input
+          ref={props.compRef}
+          className="CompanyInput TaskInput"
+          onChange={(e) => {
+            props.handleChangeCompany(e);
+          }}
+        />
+        <h4>Title</h4>
+        <input
+          ref={props.titleRef}
+          className="TitleInput TaskInput"
+          onChange={(e) => {
+            props.handleChangeTitle(e);
+          }}
+        />
+        <h4>Location</h4>
+        <input
+          ref={props.locRef}
+          className="LocationInput TaskInput"
+          onChange={(e) => {
+            props.handleChangeLocation(e);
+          }}
+        />
+        <h4>Link</h4>
+        <input
+          ref={props.linkRef}
+          className="LinkInput TaskInput"
+          onChange={(e) => {
+            props.handleChangeLink(e);
+          }}
+        />
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            props.addToList();
+            props.webRef.current.value = "LinkedIn";
+            props.compRef.current.value = "";
+            props.titleRef.current.value = "";
+            props.locRef.current.value = "";
+            props.linkRef.current.value = "";
+          }}>
+          Add
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ManualSubmitForm;
