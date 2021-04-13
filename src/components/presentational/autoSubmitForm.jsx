66babfa2a86e6b12005b9ebd77ec941d6be4ac2f
Jonathan Ko <jonathan.ko523@gmail.com>
2021-04-12 22:20:10 -0700

    Left Input Page refactored

---
+++ src/components/presentational/autoSubmitForm.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const autoSubmitForm = (props) => {
+  return (
+    <div>
+      <p className="text-center mt-4">
+        Copy and paste the url of the job page (Currently supports LinkedIn and
+        Indeed).
+      </p>
+      <div className="LeftSubBar">
+        <h4>Website</h4>
+
+        <select
+          ref={props.directWebRef}
+          className="WebsiteInput TaskInput"
+          onChange={(e) => {
+            props.handleChangeDirectWebsite(e);
+          }}>
+          <option value="Indeed">Indeed</option>
+          <option defaultValue="selected" value="LinkedIn">
+            LinkedIn
+          </option>
+          <option value="BuiltInLA">BuiltInLA</option>
+          <option value="Angelist">Angelist</option>
+        </select>
+
+        <h4>Link</h4>
+        <input
+          ref={props.directLinkRef}
+          className="CompanyInput TaskInput"
+          onChange={(e) => {
+            props.handleChangeDirectLink(e);
+          }}
+        />
+
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            props.directLinkRef.current.value = "";
+
+            if (props.directWebsiteInput === "LinkedIn") {
+              props.retrieveHtmlLinkedin();
+            } else if (props.directWebsiteInput === "Indeed") {
+              props.retrieveHtmlIndeed();
+            } else if (props.directWebsiteInput === "BuiltInLA") {
+              props.retrieveHtmlBuiltInLA();
+            } else if (props.directWebsiteInput === "Angelist") {
+              props.retrieveHtmlAngelist();
+            }
+          }}>
+          Add
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default autoSubmitForm;
--- src/components/presentational/inputpageleft.jsx
+++ src/components/presentational/inputpageleft.jsx
@@ -1,60 +1,13 @@
 import React from "react";
 import ManualSubmitForm from "./manualSubmitForm.jsx";
-
+import AutoSubmitForm from "./autoSubmitForm.jsx";
 const LeftPage = (props) => {
   //Input boxes that take in input regarding relevant information to a job application.
   return (
     <div className="leftBar col-xs-6">
       <ManualSubmitForm></ManualSubmitForm>
       <h3 className="text-center"> OR </h3>
-      <p className="text-center mt-4">
-        Copy and paste the url of the job page (Currently supports LinkedIn and
-        Indeed).
-      </p>
-      <div className="LeftSubBar">
-        <h4>Website</h4>
-
-        <select
-          ref={props.directWebRef}
-          className="WebsiteInput TaskInput"
-          onChange={(e) => {
-            props.handleChangeDirectWebsite(e);
-          }}>
-          <option value="Indeed">Indeed</option>
-          <option defaultValue="selected" value="LinkedIn">
-            LinkedIn
-          </option>
-          <option value="BuiltInLA">BuiltInLA</option>
-          <option value="Angelist">Angelist</option>
-        </select>
-
-        <h4>Link</h4>
-        <input
-          ref={props.directLinkRef}
-          className="CompanyInput TaskInput"
-          onChange={(e) => {
-            props.handleChangeDirectLink(e);
-          }}
-        />
-
-        <button
-          className="btn btn-primary"
-          onClick={() => {
-            props.directLinkRef.current.value = "";
-
-            if (props.directWebsiteInput === "LinkedIn") {
-              props.retrieveHtmlLinkedin();
-            } else if (props.directWebsiteInput === "Indeed") {
-              props.retrieveHtmlIndeed();
-            } else if (props.directWebsiteInput === "BuiltInLA") {
-              props.retrieveHtmlBuiltInLA();
-            } else if (props.directWebsiteInput === "Angelist") {
-              props.retrieveHtmlAngelist();
-            }
-          }}>
-          Add
-        </button>
-      </div>
+      <AutoSubmitForm></AutoSubmitForm>
     </div>
   );
 };
