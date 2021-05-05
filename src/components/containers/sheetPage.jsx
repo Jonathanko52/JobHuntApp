161da8c0c60933a05d9b88a35655b3558b3d2dd5
Jonathan Ko <jonathan.ko523@gmail.com>
2021-05-04 23:12:31 -0700

    Css progress

--- src/components/containers/sheetPage.jsx
+++ src/components/containers/sheetPage.jsx
@@ -7,38 +7,32 @@ const SheetPage = (props) => {
   });
 
   return (
-    <div>
-      <div className="col-xs-10 d-flex justify-content-center pt-5">
-        <div className="container">
-          {sheetcontents}
-          <div className="row">
-            <div className="text-center col-xs-5">
-              <button
-                className="btn btn-primary"
-                onClick={() => {
-                  props.getAllOfSheet(props.spreadSheetId);
-                }}>
-                getAllOfSheet
-              </button>
+    <div className="col-xs-10 d-flex justify-content-center pt-5">
+      <div className="container ">
+        {sheetcontents}
+        <div className="text-center col-xs-5">
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              props.getAllOfSheet(props.spreadSheetId);
+            }}>
+            getAllOfSheet
+          </button>
 
-              <button
-                className="btn btn-primary"
-                onClick={() => {
-                  props.updateColumnOfSheet();
-                }}>
-                update column of sheet
-              </button>
-              <p className="mt-3">
-                Your Spreadsheet Id is:<br></br>
-              </p>
-              <p className="mt-3">
-                Check out your spreadsheet yourself at:
-                <br></br>
-              </p>
-            </div>
-            <div className="col-xs-1"></div>
-            <div className="text-center col-xs-5"></div>
-          </div>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              props.updateColumnOfSheet();
+            }}>
+            update column of sheet
+          </button>
+          <p className="mt-3">
+            Your Spreadsheet Id is:<br></br>
+          </p>
+          <p className="mt-3">
+            Check out your spreadsheet yourself at:
+            <br></br>
+          </p>
         </div>
       </div>
     </div>
--- src/components/presentational/sheetrow.jsx
+++ src/components/presentational/sheetrow.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
 const SheetRow = (props) => {
-  console.log(props.data);
   return (
-    <div>
-      <div className="">{props.data[0]}</div>
-      <div className="">{props.data[1]}</div>
-      <div className="">{props.data[2]}</div>
-      <div className="">{props.data[3]}</div>
-      <div className="">{props.data[4]}</div>
-      <div className="">{props.data[5]}</div>
-      <div className="">{props.data[6]}</div>
-      <div className="">{props.data[7]}</div>
+    <div className="row border border-secondary">
+      <div className="col-xs-12">{props.data[0]}</div>
+      <div className="col-xs-12">{props.data[1]}</div>
+      <div className="col-xs-12">{props.data[2]}</div>
+      <div className="col-xs-12">{props.data[3]}</div>
+      <div className="col-xs-12">{props.data[4]}</div>
+      <div className="col-xs-12">{props.data[5]}</div>
+      <div className="col-xs-12">{props.data[6]}</div>
+      <div className="col-xs-12">{props.data[7]}</div>
     </div>
   );
 };
