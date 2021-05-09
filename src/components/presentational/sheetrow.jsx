ed0dd5fb2c46562fcf9cf6773aa53864e0e5587b
Jonathan Ko <jonathan.ko523@gmail.com>
2021-05-08 22:19:50 -0700

    workign better

--- src/components/presentational/sheetrow.jsx
+++ src/components/presentational/sheetrow.jsx
@@ -3,14 +3,17 @@ import React from "react";
 const SheetRow = (props) => {
   return (
     <div className="row border border-secondary">
-      <div className="col-3  border border-secondary">{props.data[0]}</div>
-      <div className="col-3  border border-secondary">{props.data[1]}</div>
-      <div className="col-3  border border-secondary">{props.data[2]}</div>
-      <div className="col-3  border border-secondary">{props.data[3]}</div>
-      <div className="col-3  border border-secondary">{props.data[4]}</div>
-      <div className="col-3  border border-secondary">{props.data[5]}</div>
-      <div className="col-3  border border-secondary">{props.data[6]}</div>
-      <div className="col-3 border border-secondary">{props.data[7]}</div>
+      <div className="col-1  border border-secondary">{props.data[0]}</div>
+      <div className="col-1  border border-secondary">{props.data[1]}</div>
+      <div className="col-1  border border-secondary">{props.data[2]}</div>
+      <div className="col-1  border border-secondary">{props.data[3]}</div>
+      <div className="col-1  border border-secondary">{props.data[4]}</div>
+      <div className="col-1  border border-secondary">{props.data[5]}</div>
+      <div className="col-1  border border-secondary">{props.data[6]}</div>
+      <div className="col-1 border border-secondary">
+        <a href={props.data[7]}>Link to Site</a>
+        {props.data[7]}
+      </div>
     </div>
   );
 };
