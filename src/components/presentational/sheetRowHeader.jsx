297120eb5ef14fc641f907b4fd24c27705ac36ef
Jonathan Ko <jonathan.ko523@gmail.com>
2021-05-11 22:26:59 -0700

    Added sheet row header object

---
+++ src/components/presentational/sheetRowHeader.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const SheetRowHeader = (props) => {
+  let JobPostingSource = props.data[0];
+  let Company = props.data[1];
+  let PositionTitle = props.data[2];
+  let Date = props.data[3];
+  let Location = props.data[4];
+  let CoverLetter = props.data[5];
+  let InterviewPage = props.data[6];
+  let PositionLink = props.data[7];
+  return (
+    <div className="row border border-secondary">
+      <div className="col-1  border border-secondary">{props.data[0]}</div>
+      <div className="col-1  border border-secondary">{props.data[1]}</div>
+      <div className="col-1  border border-secondary">{props.data[2]}</div>
+      <div className="col-1  border border-secondary">{props.data[3]}</div>
+      <div className="col-1  border border-secondary">{props.data[4]}</div>
+      <div className="col-1  border border-secondary">
+        <input
+          type="radio"
+          name="CoverLetter"
+          className="CoverInput RadioInput"
+          onChange={(e) => {}}
+          value="Yes"
+        />
+        <input
+          type="radio"
+          name="CoverLetter"
+          className="CoverInput RadioInput"
+          onChange={(e) => {}}
+          value="Yes"
+        />
+        {props.data[5]}
+      </div>
+      <div className="col-2  border border-secondary">
+        <input
+          type="radio"
+          name="CoverLetter"
+          className="CoverInput RadioInput"
+          onChange={(e) => {}}
+          value="Yes"
+        />{" "}
+        <input
+          type="radio"
+          name="CoverLetter"
+          className="CoverInput RadioInput"
+          onChange={(e) => {}}
+          value="Yes"
+        />
+        {props.data[6]}
+      </div>
+      <div className="col-2 border border-secondary">
+        <a href={PositionLink}>Link to Site</a>
+      </div>
+    </div>
+  );
+};
+
+export default SheetRowHeader;
