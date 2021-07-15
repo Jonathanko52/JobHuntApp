85e72cf0a99ec3939d8ba36295b067b908271117
Jonathan Ko <jonathan.ko523@gmail.com>
2021-07-14 17:40:39 -0700

    get all of sheet fix

--- src/components/containers/app.jsx
+++ src/components/containers/app.jsx
@@ -1020,8 +1020,6 @@ class App extends React.Component {
     let tempDate1 = new Date();
     let tempDate2 = tempDate1.getDate() - parseInt(sheetParameters);
     let previousMonth = false;
-    console.log("tempDate1", tempDate1);
-    console.log("tempDate2", tempDate2);
 
     if (tempDate2 < 0) {
       tempDate1.setDate(1);
