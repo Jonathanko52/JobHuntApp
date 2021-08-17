d99b625ef353e4dcebe90e44b0ff9c3235f6997b
Jonathan Ko <jonathan.ko523@gmail.com>
2021-08-16 22:36:08 -0700

    Undid that

--- src/components/containers/app.jsx
+++ src/components/containers/app.jsx
@@ -45,11 +45,6 @@ class App extends React.Component {
       tempSpreadsheetID: "",
       fullSheetData: [],
       sheetParameters: "7",
-      handleAdditionalInput1: "",
-      handleAdditionalInput2: "",
-      handleAdditionalInput3: "",
-      handleAdditionalInput4: "",
-      handleAdditionalInput5: "",
     };
 
     //Event Listener Function Bindings
