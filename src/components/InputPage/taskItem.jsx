afa8ef185120fff8f709679b33a1043a2bfee59b
Jonathan Ko <jonathan.ko523@gmail.com>
2021-09-13 18:13:02 -0700

    Adding priority field

--- src/components/InputPage/taskItem.jsx
+++ src/components/InputPage/taskItem.jsx
@@ -42,6 +42,15 @@ const TaskItem = (props) => (
       {props.linkInput}
     </a>
     <br />
+    <b href> Priority</b>
+    <select>
+      <option value="5">5</option>
+      <option value="4">4</option>
+      <option value="3">3</option>
+      <option value="2">2</option>
+      <option value="1">1</option>
+    </select>
+    <br />
     <button
       className="btn btn-primary"
       onClick={() => {
