cdfe2121cc91fbc3ba811cc4e589d693574af43a
Jonathanko52 <jonathan.ko523@gmail.com>
2024-06-25 18:56:25 -0700

    fixing button row

--- src/components/InputPage/inputpageright.jsx
+++ src/components/InputPage/inputpageright.jsx
@@ -89,62 +89,64 @@ class RightPage extends React.Component {
           Number of Jobs Applied to Today: {this.state.numberAppliedToday}
         </h4>
   
-        <div className="row d-flex justify-content-center">
-        <button
-            className="btn btn-primary"
-            onClick={() => {
-              // console.log(this.props.tasks)
-              console.log("TEST", this.increaseNumberAppliedToday())
-              
-            }}>
-            log state
-          </button>
-          <button
-            className="btn btn-primary"
-            onClick={() => {
-              this.props.clearList();
-            }}>
-            Clear List
-          </button>
-          <button
-            className="btn btn-primary"
-            onClick={() => {
-              let addToList = false;
-              addToList = window.confirm(
-                "Are you sure you want to save the jobs you haven't applied to yet to sheets?"
-              );
-              if (addToList) {
-                this.props.saveTasklistToGoogleUnapplied();
-              }
-            }}>
-            Save To Google
-          </button>
-          <button
-            className="btn btn-primary"
-            onClick={() => {
-              let addToList = false;
-              addToList = window.confirm(
-                "Do you want to load your unapplied positions from sheets?"
-              );
-              if (addToList) {
-                this.props.loadTasklistFromGoogleUnapplied();
-              }
-            }}>
-            Load From Google
-          </button>
-          <button
-            className="btn btn-primary"
-            onClick={() => {
-              let addToList = false;
-              addToList = window.confirm(
-                "Are you sure you want to wipe the google unapplied list?"
-              );
-              if (addToList) {
-                this.props.clearTasklistFromGoogleUnapplied();
-              }
-            }}>
-            Clear Google
-          </button>
+        <div className="row">
+          <div className="d-flex justify-content-center" id="BUtton Row">
+            <button
+                className="btn btn-primary"
+                onClick={() => {
+                  // console.log(this.props.tasks)
+                  console.log("TEST", this.increaseNumberAppliedToday())
+                  
+                }}>
+                log state
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  this.props.clearList();
+                }}>
+                Clear List
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  let addToList = false;
+                  addToList = window.confirm(
+                    "Are you sure you want to save the jobs you haven't applied to yet to sheets?"
+                  );
+                  if (addToList) {
+                    this.props.saveTasklistToGoogleUnapplied();
+                  }
+                }}>
+                Save To Google
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  let addToList = false;
+                  addToList = window.confirm(
+                    "Do you want to load your unapplied positions from sheets?"
+                  );
+                  if (addToList) {
+                    this.props.loadTasklistFromGoogleUnapplied();
+                  }
+                }}>
+                Load From Google
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  let addToList = false;
+                  addToList = window.confirm(
+                    "Are you sure you want to wipe the google unapplied list?"
+                  );
+                  if (addToList) {
+                    this.props.clearTasklistFromGoogleUnapplied();
+                  }
+                }}>
+                Clear Google
+              </button>
+          </div>
         </div>
         <div className="row"></div>
         <ol className="TaskTable">{Array}</ol>
